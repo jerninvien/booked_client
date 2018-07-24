5a2c1c2d36b163ffdb5cd63a93794d88155eada8
Ramanujan <bluushuuz@gmail.com>
2018-07-24 14:25:34 -0700

    Minor cleanup

--- index.js
+++ index.js
@@ -2,15 +2,12 @@
 
 import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from 'app/src/App';
-import {name as appName} from './app.json';
-
 import { Provider } from 'react-redux';
-// import configureStore from 'app/src/configureStore';
-// const store = configureStore();
-
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+
+import App from 'app/src/App';
+import { name as appName } from './app.json';
 import appStore from 'app/src/redux';
 
 const configureStore = () => {
--- src/configureStore.js
+++
@@ -1,7 +0,0 @@
-// import { createStore } from 'redux';
-// import app from './redux';
-//
-// export default function configureStore() {
-//   let store = createStore(app);
-//   return store;
-// }
